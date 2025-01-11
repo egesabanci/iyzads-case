5dcf364053d18efbcd02f698fb0d874776c00163
Ege Sabancı <egesabanci@outlook.com.tr>
2025-01-11 04:01:03 +0300

    chore: minor docker-config update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
