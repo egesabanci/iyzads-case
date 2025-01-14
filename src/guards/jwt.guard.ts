87de29e4b27299b171af0defb309dbfe5d9a7f6c
Ege Sabancı <egesabanci@outlook.com.tr>
2025-01-14 20:43:48 +0300

    refactor: minor

--- src/guards/jwt.guard.ts
+++ src/guards/jwt.guard.ts
@@ -42,7 +42,6 @@ export class JWTGuard implements CanActivate {
       });
 
       request.user = payload;
-      console.log(request.user);
       return true;
     } catch (error) {
       throw new UnauthorizedException(error.message);
